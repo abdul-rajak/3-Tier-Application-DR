820b8a1c8d31a97dbd53f6a5dfc321aab41c59a1
mohdzakir12 <zakir.mohd@synectiks.com>
2023-09-17 11:23:58 +0000

    fixed recipe packages installation depencies

--- cookbooks/PetClinic-App/recipes/default.rb
+++ cookbooks/PetClinic-App/recipes/default.rb
@@ -9,13 +9,11 @@ package jdk_package do
   action :install
 end
 
-
 log 'java_installation_complete' do
   message 'Java installation is complete.'
   level :info  # You can choose the log level (e.g., :info, :warn, :error) based on your preference.
   action :write
 end
-
 
 # Create a Maven directory
 directory '/opt/maven' do
@@ -31,18 +29,9 @@ maven_extract_dir = "/opt/apache-maven"
 #maven_version = '3.8.4'
 maven_url = "https://dlcdn.apache.org/maven/maven-3/3.9.4/binaries/apache-maven-3.9.4-bin.tar.gz"
 
-# # Download and extract Maven
-# remote_file '/tmp/apache-maven.tar.gz' do
-#   source maven_url
-#   owner 'root'
-#   group 'root'
-#   mode '0644'
-#   action :create
-# end
 maven_home = '/opt/maven'
 maven_bin = "#{maven_home}/bin"
 maven_cmd = 'mvn'
-
 
 execute 'download_maven' do
   command "wget #{maven_url} -O /tmp/apache-maven.tar.gz"
@@ -51,10 +40,7 @@ execute 'download_maven' do
   not_if { ::File.exist?("#{maven_bin}/#{maven_cmd}") }
 end
 
-
-
 # Define environment variables for Maven
-
 # Extract Maven and set environment variables
 execute 'extract_maven' do
   command "tar xzf /tmp/apache-maven.tar.gz -C #{maven_home} --strip-components=1"
@@ -69,16 +55,11 @@ link '/usr/bin/mvn' do
   to "#{maven_bin}/#{maven_cmd}"
 end
 
-
-
-
 log 'Maven_installation_complete' do
   message 'maven installation is complete.'
   level :info  # You can choose the log level (e.g., :info, :warn, :error) based on your preference.
   action :write
 end
-
-
 
  #Define the target directory where you want to clone the repository
 target_directory = '/home/ubuntu/spring-petclinic-rest'
@@ -94,25 +75,11 @@ directory target_directory do
   action :create
 end
 
-# Clone the Git repository as the root user
-#execute 'clone_repository' do
-  #command "cd  #{target_directory} && git clone #{repository_url}"
-  #user 'root'
-  #group 'root'
-  #umask '022'  # Adjust the umask as needed
- # action :run
-#end
-
 git target_directory do
-
   repository repository_url
-
   revision 'master'
-
   action :sync
-
 end
-
 
 log 'repo_clone_complete' do
   message 'pet-clinic repo clone is complete.'
@@ -121,89 +88,21 @@ log 'repo_clone_complete' do
 end
 
 # Configurations of applications
-
 template '/home/ubuntu/spring-petclinic-rest/src/main/resources/application-postgresql.properties' do
   source 'appconfigfile.erb'
   variables( DB_host: dbconfigdata['DB_host'],
             DB_user: dbconfigdata['DB_user'], 
             DB_password: dbconfigdata['DB_password'] )
+  notifies :run, 'execute[run_maven_build]', :delayed
+  notifies :restart, 'service[petclinic]', :delayed
 end
-
-=begin
-old code - this is working commented this to accomodate templates
-ruby_block 'update_postgresql_config' do
-    block do
-      # Create the JDBC URL by replacing the host placeholder
-      #url = 'jdbc:postgresql://#{node['petclinic-testing']['DB_host']}:5432/petclinic'
-      #username=node['petclinic-testing']['DB_user']
-      #password=node['petclinic-testing']['DB_password']
-      url = "jdbc:postgresql://#{node['PetClinic-App']['DB_host']}:5432/petclinic"
-      username = node['PetClinic-App']['DB_user']
-      password = node['PetClinic-App']['DB_password']
-      # Define the new lines with variables
-      new_lines = <<-EOL
-  spring.sql.init.schema-locations=classpath*:db/postgresql/initDB.sql
-  spring.sql.init.data-locations=classpath*:db/postgresql/populateDB.sql
-
-  # PostgreSQL config start
-  #----------------------------------------------------------------
-  spring.datasource.url=#{url}
-  spring.datasource.username=#{username}
-  spring.datasource.password=#{password}
-  spring.datasource.driver-class-name=org.postgresql.Driver
-  spring.jpa.database=POSTGRESQL
-  spring.jpa.database-platform=org.hibernate.dialect.PostgreSQLDialect
-  spring.jpa.hibernate.ddl-auto=update
-  #----------------------------------------------------------------
-  # PostgreSQL config end
-      EOL
-
-      # Define the configuration file path
-      config_file = '/home/ubuntu/spring-petclinic-rest/src/main/resources/application-postgresql.properties'
-
-      # Check if the file exists before making changes
-      if File.exist?(config_file)
-        # Replace the contents of the configuration file with new_lines
-        File.open(config_file, 'w') { |file| file.write(new_lines) }
-      else
-        raise "Error: Configuration file '#{config_file}' not found."
-      end
-    end
-    action :run
-  end
-=end
 
 template '/home/ubuntu/spring-petclinic-rest/src/main/resources/application.properties' do
   source 'application_properties.erb'
   variables( DB_database: dbconfigdata['DB_database'])
+  notifies :run, 'execute[run_maven_build]', :delayed
+  notifies :restart, 'service[petclinic]', :delayed
 end
-
-=begin
-  # Define recipe to change application.properties
-  ruby_block 'update_application_properties' do
-    block do
-      # Define the path to application.properties
-      config_file_2 = '/home/ubuntu/spring-petclinic-rest/src/main/resources/application.properties'
-
-      # Check if the file exists before making changes
-      if File.exist?(config_file_2)
-        # Use sed to replace "hsqlb" with "postgresql" in line 19
-        system("sed -i '19s/hsqldb/postgresql/' #{config_file_2}")
-
-        # Check if the sed command was successful
-        unless $?.success?
-          raise "Error: Failed to update line 19 in '#{config_file_2}'."
-        end
-      else
-        raise "Error: Configuration file '#{config_file_2}' not found."
-      end
-    end
-    action :run
-  end
-=end
-
-
-
 
 log 'configuration_complete' do
   message 'pet-clinic configurations is complete.'
@@ -211,28 +110,18 @@ log 'configuration_complete' do
   action :write
 end
 
-
-execute 'remove_directory' do
-  command 'rm -rf /home/ubuntu/spring-petclinic-rest/target'
-  user 'root'
-  action :run
-end
-
 # Define the command to run
-mvn_command = 'mvn clean install -DskipTests=true'
+mvn_command = 'rm -rf /home/ubuntu/spring-petclinic-rest/target && mvn clean install -DskipTests=true'
 
 # Use the execute resource to run the command with sudo as the root user
 execute 'run_maven_build' do
   command "sudo #{mvn_command}"
   cwd '/home/ubuntu/spring-petclinic-rest' # Specify the path to your Maven project
   user 'root'
-  action :run
+  action :nothing
 end
 
-
 # creating service for pet clinic
-
-
 # Define the unit file path
 unit_file_path = '/etc/systemd/system/petclinic.service'
 
@@ -265,8 +154,7 @@ service 'petclinic' do
   action [:enable, :start]
 end
 
-
 # Restart the petclinic service
 service 'petclinic' do
-  action :restart
+  action :nothing
 end
--- cookbooks/PetClinic-Web/recipes/default.rb
+++ cookbooks/PetClinic-Web/recipes/default.rb
@@ -17,25 +17,21 @@ package 'nodejs' do
   not_if 'dpkg -l | grep -E "^ii\s+nodejs\b"'  # Check if "nodejs" package is installed
 end
 
-
 # Use the execute resource to update npm to the latest version
 execute 'update_npm' do
   command 'npm install -g npm@latest'
   user 'root'
   action :run
+  not_if 'npm -v | grep -q "^10"'
 end
-
-#install angular
-#angular_cli
 
 # Install the latest version of the Angular CLI globally
 execute 'install_angular_cli' do
   command 'npm install -g @angular/cli@latest'
   user 'root'
   action :run
+  not_if 'ng version | grep -q "^Angular:"'
 end
-
-# recipes/install_nginx.rb
 
 # Install Nginx
 package 'nginx' do
@@ -59,15 +55,6 @@ directory target_directory do
   action :create
 end
 
-# Clone the Git repository as the root user
-#execute 'clone_repository' do
-  #command "cd  #{target_directory} && git clone #{repository_url}"
-  #user 'root'
-  #group 'root'
-  #umask '022'  # Adjust the umask as needed
- # action :run
-#end
-
 git target_directory do
   repository repository_url
   revision 'master'
@@ -77,85 +64,35 @@ end
 template '/home/ubuntu/spring-petclinic-angular/src/environments/environment.ts' do
   source 'environment.ts.erb'
   variables( APP_host: appconfigdata['APP_host'])
+  notifies :run, 'execute[npm_install]', :delayed
+  notifies :run, 'execute[ng_build]', :delayed
 end
-
-=begin
-config_file = '/home/ubuntu/spring-petclinic-angular/src/environments/environment.ts'
-host = node['PetClinic-Web']['App_host']
-
-# Specify the updated REST_API_URL with the desired URL
-new_api_url = "'http://#{host}:9966/petclinic/api/'"
-
-# Read the content of the configuration file
-ruby_block 'update_environment_config' do
-  block do
-    file_content = ::File.read(config_file)
-
-    # Replace the old REST_API_URL value with the new value
-    updated_content = file_content.gsub(/'http:\/\/.*?'/, new_api_url)
-
-    # Write the updated content back to the configuration file
-    File.open(config_file, 'w') { |file| file.write(updated_content) }
-  end
-  action :run
-end
-=end
 
 # recipes/build_angular_app.rb
-
 # Define the path to your Angular project directory
 angular_project_dir = '/home/ubuntu/spring-petclinic-angular/'
-
 
 execute 'npm_install' do
   command 'npm install'
   cwd angular_project_dir  # Set the working directory to the project directory
-  action :run
+  action :nothing
 end
 
 # Execute the ng build command
 execute 'ng_build' do
   command "cd #{angular_project_dir} && ng build"
   user 'root'  # Replace with the username that should run the command
-  action :run
+  action :nothing
 end
-
-
 
 # recipes/configure_nginx.rb
 template '/etc/nginx/sites-enabled/petclinic' do
   source 'petclinic.erb'
   variables( WEB_dns: webconfigdata['WEB_dns'])
+  notifies :reload, 'service[nginx]', :delayed
 end
-
-=begin
-# Define the path to your Nginx configuration file
-nginx_config_file = '/etc/nginx/sites-enabled/petclinic'
-
-# Specify the Nginx server block content
-nginx_config_content = <<-CONFIG
-server {
-    listen 80;
-    server_name dr-web1.synectiks.net;  # Your custom IP or domain
-
-    root /home/ubuntu/spring-petclinic-angular/dist;
-    index index.html;
-
-    location / {
-        try_files $uri $uri/ /index.html;
-    }
-}
-CONFIG
-
-# Use the file resource to create the Nginx configuration file and set its content
-file nginx_config_file do
-  content nginx_config_content
-  mode '0644'  # Adjust permissions as needed
-  action :create
-end
-=end
 
 # Use the service resource to reload Nginx
 service 'nginx' do
-  action :reload
+  action :nothing
 end
