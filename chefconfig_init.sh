86348c8372269c00b4139c14a6ffa8fda6c819e8
root <root@ip-10-0-21-201.us-west-1.compute.internal>
2023-09-18 16:08:12 +0000

    chefconfig_update

--- chefconfig_init.sh
+++ chefconfig_init.sh
@@ -3,7 +3,7 @@
 repo_path="/home/ubuntu/Dr-Chef-Git-Repo"
 # Change to the local repository directory
 cd "$repo_path" || exit
-    echo "Synchronization of cookbooks,databags and roles..."
+    echo "Upload cookbooks,data bags,roles and add runlist ..."
     # pull changes from the remote master branch
     git pull origin main
     # upload the cookbook
@@ -22,4 +22,9 @@ cd "$repo_path" || exit
     knife node run_list add DBNode role['dr_role']
     knife node run_list add WebNode role['dr_role']
     knife node run_list add AppNode role['dr_role']
+    # add runlist for dbnode,webnode and appnode
+    knife node run_list add DBNode 'recipe[db-cookbook::default]'
+    knife node run_list add AppNode 'recipe[PetClinic-App::default]'
+    knife node run_list add WebNode 'recipe[PetClinic-Web::default]'
+    # exit the script    
     exit 0
